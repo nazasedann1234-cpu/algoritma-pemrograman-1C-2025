1e89468e1bd17d0839b526dd4b6b08074fa2d56f
MAHA <maharizka4@gmail.com>
2025-10-08 12:00:11 +0700

    menambahkan ulang codingan modul 1

---
+++ modul-1/250441100119_maha/SOAL_NO3.py
@@ -0,0 +1,7 @@
+bola_merah = 8
+bola_biru = 6
+total_bola = 8 + 6 
+diambil = 3
+
+jumlah = (total_bola * total_bola - 1) * (total_bola - 2 ) // (3 * 2 * 1) 
+print("Jumlah kemungkinan dalam mengambil bola adalah", jumlah)
