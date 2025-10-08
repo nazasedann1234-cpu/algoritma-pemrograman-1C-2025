e857b8d825120486347065bcf3db13dd55ae3dfe
Muhammad Iqbal Faza <123388280+lintingggg@users.noreply.github.com>
2025-10-08 14:22:40 +0700

    Delete modul-1/soal4.py

--- modul-1/soal4.py
+++
@@ -1,6 +0,0 @@
-masa_studi = 8
-ipk = 3.00
-ada_nilai_e = False
-tidak_ada_nilai_e = True
-#print(masa_studi >= 8 and ipk >= 3.00 and ada_nilai_e)
-print(masa_studi <= 8 and ipk >= 3.00 and tidak_ada_nilai_e)
