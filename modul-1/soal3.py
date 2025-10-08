92137c7cb586805c682b524290ec575d7c6f1ca9
Muhammad Iqbal Faza <123388280+lintingggg@users.noreply.github.com>
2025-10-08 14:22:27 +0700

    Delete modul-1/soal3.py

--- modul-1/soal3.py
+++
@@ -1,8 +0,0 @@
-soal3 = "Tanpa menggunakan kalkulator atau menjalankan kode, hitunglah hasil akhir dari variabel hasil pada operasi aritmatika di bawah ini. Tunjukkan urutan perhitungannya"
-print(soal3)
-
-print()
-
-hasil = 10 + 5 * 2 - 12 / 6 % 3
-
-print(hasil)
